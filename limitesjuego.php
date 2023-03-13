e227d1207f0fed8b32fbdf06ab2b9309b6a8f7df
unknown <Vicky>
2023-03-13 11:14:04 +0100

    Modificar el title en limitesJuego desde master

--- limitesjuego.php
+++ limitesjuego.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Adivina el número secreto</title>
+        <title>Juego de master</title>
         <meta charset="UTF-8">
         <link rel="stylesheet" href="stylesheet.css">
     </head>
